2f7159280b4a763a981945f7dadc1cfdf0dc9d99
oscar <oscar.caasmontes@gmail.com>
2024-12-23 10:56:42 +0100

    Validaciones entrada morse y texto

---
+++ src/entities.py
@@ -0,0 +1,3 @@
+class Morse:
+    def __init__(self):
+        pass
--- src/interface.py
+++ src/interface.py
@@ -1,5 +1,17 @@
 import tkinter as tk
 
+def add_to_morse(widget, content):
+    widget.config(state="normal")
+    widget.insert("end", content)
+    widget.config(state="disabled") 
+
+def filter_plain_input(event, widget):
+    allowed_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "
+    content = widget.get("1.0", "end-1c")
+    filtered_content = "".join([char for char in content.upper() if char in allowed_characters])
+    widget.delete("1.0", "end") 
+    widget.insert("1.0", filtered_content)
+
 def menu():
     root = tk.Tk()
     root.title("Traductor Morse")
@@ -33,7 +45,7 @@ def menu():
     label_morse.pack(side="top", pady=10, anchor="w", padx=5)
 
     # Área de texto para Morse
-    text_morse = tk.Text(frame_morse, wrap="word", font=("Arial", 14), bd=1, relief="solid", height=15)
+    text_morse = tk.Text(frame_morse, wrap="word", font=("Arial", 14), bd=1, relief="solid", height=15, state="disabled")
     text_morse.pack(side="top", expand=True, fill="both", padx=10)
 
     # Frame para los botones debajo del área de texto Morse
@@ -41,11 +53,14 @@ def menu():
     frame_buttons.pack(side="top", fill="x", padx=10, pady=(0, 10))
 
     # Botones para Morse
-    button_punto = tk.Button(frame_buttons, text="•", font=("Arial", 14), bd=1, relief="solid", width=5, height=2)
+    button_punto = tk.Button(frame_buttons, text="•", font=("Arial", 14), bd=1, relief="solid", width=5, 
+                             height=2, command=lambda: add_to_morse(text_morse, "•"))
     button_punto.pack(side="left", expand=True, fill="both")
-    button_raya = tk.Button(frame_buttons, text="–", font=("Arial", 14), bd=1, relief="solid", width=5, height=2)
+    button_raya = tk.Button(frame_buttons, text="–", font=("Arial", 14), bd=1, relief="solid", width=5, 
+                            height=2, command=lambda: add_to_morse(text_morse, "–"))
     button_raya.pack(side="left", expand=True, fill="both")
-    button_pausa = tk.Button(frame_buttons, text="pausa", font=("Arial", 14), bd=1, relief="solid", width=5, height=2)
+    button_pausa = tk.Button(frame_buttons, text="pausa", font=("Arial", 14), bd=1, relief="solid", width=5, 
+                             height=2, command=lambda: add_to_morse(text_morse, " "))
     button_pausa.pack(side="left", expand=True, fill="both")
 
     # Frame para las flechas de conversión
@@ -69,6 +84,7 @@ def menu():
     # Área de texto para Plano
     text_plain = tk.Text(frame_plain, wrap="word", font=("Arial", 14), bd=1, relief="solid", height=15)
     text_plain.pack(side="top", expand=True, fill="both", padx=10, pady=(0, 10))
+    text_plain.bind("<KeyRelease>",  lambda event: filter_plain_input(event, text_plain))
 
 
     # Ejecutar la aplicación
